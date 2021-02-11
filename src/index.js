465df57bfcd48d434a9b637900a3b303f51ae01a
unknown <maor30levy@gmail.com>
2021-02-11 14:54:30 +0200

    2.0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const hbs = require('hbs');
 const path = require('path');
-const port = process.env.PORT | 3000;
+const port = process.env.PORT | 80;
 const app = express();
 
 
